47929cb44ad557d7e908e7a1f9a292b606d6327c
Isaac Brown <ikebenbrown@gmail.com>
2018-02-12 16:27:07 -0600

    Added methods to ShuffleboardManager

--- src/com/team2169/util/ShuffleBoardManager.java
+++ src/com/team2169/util/ShuffleBoardManager.java
@@ -1,13 +1,34 @@
 package com.team2169.util;
 
 import com.team2169.robot.Robot;
-
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+@SuppressWarnings("deprecation")
 public class ShuffleBoardManager {
+
+	NetworkTable dash = NetworkTable.getTable("SmartDashboard");
+
+	public void setDouble(String key, double D){
+		dash.putDouble(key, D);
+	}
+
+	public void getDouble(String key){
+		dash.getNumber(key, 0);
+	}
 	
 	
-	public void init(boolean fms) {		
+	public void init(boolean fms) {	
+		setDouble("maxVelocity", 0);
+		setDouble("pathFinderD", 0);
+		setDouble("accelerationGain", 0);
+		setDouble("timeStep", 0);
+		setDouble("pathfinderP", 0);
+		setDouble("pathfinderI", 0);
+		setDouble("maxAcceleration", 0);
+		setDouble("maxJerk", 0);
+		
+		
 		
 		if(fms) {
 			//FMS Is Attached
