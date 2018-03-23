ec384e4c434f04d688793c44928690602bade053
Unknown <devosmaxwell@gmail.com>
2018-03-23 11:48:53 -0500

    Imported and created CSV Parser

--- src/com/team2169/util/PathStorageHandler.java
+++ src/com/team2169/util/PathStorageHandler.java
@@ -127,6 +127,15 @@ public class PathStorageHandler {
 				CSVReader readerLeft = new CSVReaderBuilder(brLeft).withCSVParser(parserLeft).build()) {
 			
 			//Pull Left Data from CSV
+			//Convert to actual reader
+			String[] nextRecord;
+            while ((nextRecord = readerLeft.readNext()) != null) {
+                System.out.println("Name : " + nextRecord[0]);
+                System.out.println("Email : " + nextRecord[1]);
+                System.out.println("Phone : " + nextRecord[2]);
+                System.out.println("Country : " + nextRecord[3]);
+                System.out.println("==========================");
+            }
 			
 
 		} catch (IOException e) {
@@ -141,6 +150,16 @@ public class PathStorageHandler {
 			
 			
 			//Pull Right Data from CSV
+			//Convert to actual reader
+			String[] nextRecord;
+            while ((nextRecord = readerRight.readNext()) != null) {
+                System.out.println("Name : " + nextRecord[0]);
+                System.out.println("Email : " + nextRecord[1]);
+                System.out.println("Phone : " + nextRecord[2]);
+                System.out.println("Country : " + nextRecord[3]);
+                System.out.println("==========================");
+            }
+			
 			
 
 		} catch (IOException e) {
