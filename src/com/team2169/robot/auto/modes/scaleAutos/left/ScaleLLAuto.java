76ff77da88eb8155d3725645e22b89e63d7f37c2
Unknown <devosmaxwell@gmail.com>
2018-04-03 14:10:47 -0500

    Added Same-Side Scale Autos

--- src/com/team2169/robot/auto/modes/scaleAutos/left/ScaleLLAuto.java
+++ src/com/team2169/robot/auto/modes/scaleAutos/left/ScaleLLAuto.java
@@ -1,8 +1,21 @@
 package com.team2169.robot.auto.modes.scaleAutos.left;
 
+import java.util.Arrays;
+
 import com.team2169.robot.RobotStates;
 import com.team2169.robot.RobotStates.RunningMode;
+import com.team2169.robot.auto.AutoConstants;
 import com.team2169.robot.auto.modes.AutoMode;
+import com.team2169.robot.auto.tasks.ParallelTask;
+import com.team2169.robot.auto.tasks.Task;
+import com.team2169.robot.auto.tasks.arm.ArmRetract;
+import com.team2169.robot.auto.tasks.drive.DriveStraight;
+import com.team2169.robot.auto.tasks.drive.TurnInPlace;
+import com.team2169.robot.auto.tasks.elevator.ElevatorToScaleHigh;
+import com.team2169.robot.auto.tasks.elevator.ElevatorToSwitch;
+import com.team2169.robot.auto.tasks.intake.IntakeExhaust;
+
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class ScaleLLAuto extends AutoMode {
 /*
@@ -31,6 +44,19 @@ public class ScaleLLAuto extends AutoMode {
     public ScaleLLAuto() {
 
         RobotStates.runningMode = RunningMode.AUTO;
+        addSequential(new ParallelTask(Arrays.asList(new Task[] {
+                new DriveStraight(AutoConstants.LeftAutos.ScaleAutos.LeftScale.startToPoint, .5),
+                new ArmRetract(),
+                new ElevatorToSwitch(),        
+        })));
+        addSequential(new ParallelTask(Arrays.asList(new Task[] {
+        		new TurnInPlace(.5, AutoConstants.LeftAutos.ScaleAutos.LeftScale.pointToScaleTurn),
+                new ArmRetract(),
+                new ElevatorToScaleHigh(),        
+        })), 2);
+        addSequential(new DriveStraight(AutoConstants.LeftAutos.ScaleAutos.LeftScale.pointToScale, .5));
+        addSequential(new WaitCommand(1));
+        addSequential(new IntakeExhaust(AutoConstants.LeftAutos.ScaleAutos.LeftScale.intakeSpeed, true), 2);
 
 
 
