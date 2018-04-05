b793d3b632e19919c8788f1fb9c9876c6fc31bfe
Unknown <devosmaxwell@gmail.com>
2018-04-05 12:50:38 -0500

    Created Self-Test

--- src/com/team2169/robot/auto/modes/SelfTest.java
+++ src/com/team2169/robot/auto/modes/SelfTest.java
@@ -2,21 +2,23 @@ package com.team2169.robot.auto.modes;
 
 import com.team2169.robot.RobotStates;
 import com.team2169.robot.RobotStates.RunningMode;
-import com.team2169.robot.auto.AutoConstants;
+import com.team2169.robot.auto.tasks.arm.ArmRetract;
 import com.team2169.robot.auto.tasks.drive.DriveStraight;
 import com.team2169.robot.auto.tasks.drive.TurnInPlace;
+import com.team2169.robot.auto.tasks.elevator.ElevatorToSwitch;
 import com.team2169.robot.auto.tasks.intake.IntakeExhaust;
-
-import edu.wpi.first.wpilibj.DriverStation;
 
 public class SelfTest extends AutoMode {
 
     public SelfTest() {
-        DriverStation.reportError("AUTOMODE - SELF", false);
-        addSequential(new DriveStraight(AutoConstants.CenterAutos.SwitchAutos.LeftSwitch.startToPoint, .5));
-        addSequential(new TurnInPlace(.5, AutoConstants.CenterAutos.SwitchAutos.LeftSwitch.pointToSwitchTurn), 2);
-        addSequential(new DriveStraight(AutoConstants.CenterAutos.SwitchAutos.LeftSwitch.pointToSwitch, .5));
-        addSequential(new IntakeExhaust(.75, true), 2);
+    	
+    	this.autoName = "Self Test";
+        addSequential(new ElevatorToSwitch());
+        addSequential(new ArmRetract());
+    	addSequential(new IntakeExhaust(.5, true), 2);
+    	addSequential(new DriveStraight(24, .5));
+    	addSequential(new TurnInPlace(.25, 30));
+
     }
 
     public void looper() {
